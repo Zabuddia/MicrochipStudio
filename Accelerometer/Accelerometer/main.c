e5be176c146320d8fd3af0264d8f8b6fb7aea3e0
Zabuddia <141687084+Zabuddia@users.noreply.github.com>
2024-05-09 12:35:34 -0600

    Got serial communication to work

---
+++ Accelerometer/Accelerometer/main.c
@@ -0,0 +1,18 @@
+/*
+ * Accelerometer.c
+ *
+ * Created: 5/9/2024 12:22:45 PM
+ * Author : fifea
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
--- SerialCommunication/SerialCommunication/main.c
+++ SerialCommunication/SerialCommunication/main.c
@@ -5,60 +5,23 @@
  * Author : fifea
  */ 
 
+#define PRESCALAR 6
+
 #include <avr/io.h>
-#define F_CPU 16000000UL
+#include <avr/wdt.h>
+#define F_CPU (16000000UL / PRESCALAR) //~2.667MHz
 #include <util/delay.h>
 
 #define BAUD_RATE 9600
 #define S 16UL
 #define BAUD_SETTING ((64 * F_CPU) / (BAUD_RATE * S))
 
-#define RED_LED 0
-#define GREEN_LED 1
-#define IGN_1 1
-#define IGN_2 4
-#define HORN 3
-#define PULLUPEN 3
-
-#define DELAY 1000
+#define DELAY 500
 
 #define SET_BIT(r, b) (r |= (0x01 << b))
 #define CLR_BIT(r, b) (r &= ~(0x01 << b))
 #define TGL_BIT(r, b) (r ^= (0x01 << b))
 #define GET_BIT(r, b) ((r >> b) & 0x01)
-
-//Set buttons as inputs
-void BTN_Init(void) {
-	SET_BIT(PORTD.DIRCLR, IGN_1);
-	SET_BIT(PORTF.DIRCLR, IGN_2);
-	SET_BIT(PORTA.DIRCLR, HORN);
-	//SET_BIT(PORTD.PIN1CTRL, PULLUPEN);
-	//SET_BIT(PORTF.PIN4CTRL, PULLUPEN);
-	//SET_BIT(PORTA.PIN3CTRL, PULLUPEN);
-}
-
-uint8_t BTN_Pressed(uint8_t btn) {
-	uint8_t pressed = 0;
-	switch (btn)
-	{
-	case IGN_1:
-		pressed = GET_BIT(PORTD.IN, IGN_1);
-		break;
-	case IGN_2:
-		pressed = GET_BIT(PORTF.IN, IGN_2);
-		break;
-	case HORN:
-		pressed = GET_BIT(PORTA.IN, HORN);
-		break;
-	}
-	return pressed;
-}
-
-//Set LEDs as outputs
-void LED_Init(void) {
-	SET_BIT(PORTF.DIRSET, RED_LED);
-	SET_BIT(PORTF.DIRSET, GREEN_LED);
-}
 
 //Set Tx and Rx and initialize UART
 void USART1_Init(void) {
@@ -67,7 +30,7 @@ void USART1_Init(void) {
 	baud_reg_val *= (1024 + sigrow_val);
 	baud_reg_val /= 1024;
 	
-	USART1.BAUD = (uint16_t)1100;
+	USART1.BAUD = (uint16_t)baud_reg_val;
 	PORTC.DIRSET = PIN0_bm;                 // Set PC0 as output
 	PORTC.DIRCLR = PIN1_bm;                 // Set PC1 as input
 	
@@ -87,11 +50,9 @@ uint8_t USART1_Receive(void) {
 
 int main(void) {
 	USART1_Init();
-	LED_Init();
-	BTN_Init();
 	while (1) {
-		if (BTN_Pressed(IGN_2)) {
-			USART1_Transmit('I');
-		}
+		USART1_Transmit('U');
+		_delay_ms(DELAY);
+		wdt_reset();
 	}
 }
